7a5f3a5ae41718496c32b7bf486e51417c764cae
Pos <nichlasbach1000@gmail.com>
2022-10-17 11:23:16 +0200

    Commit 	# Please enter the commit message for your changes. Lines starting

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const parseUrl = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+const mongoose = require('mongoose');
+const { resolveSoa } = require('dns');
+const fs = require("fs");
+
+dotenv.config();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./views'));
+
+app.get('/', (req, res) => {
+    res.render('./views');
+});
